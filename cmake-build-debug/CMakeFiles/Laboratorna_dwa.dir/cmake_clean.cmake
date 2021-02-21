738fd541c532d2822fe5ee319a23a0999155dc19
Zepee <ramp8a799@gmail.com>
2021-02-21 20:04:09 +0200

    Project added

---
+++ cmake-build-debug/CMakeFiles/Laboratorna_dwa.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Laboratorna_dwa.dir/main.cpp.obj"
+  "Laboratorna_dwa.exe"
+  "Laboratorna_dwa.exe.manifest"
+  "Laboratorna_dwa.pdb"
+  "libLaboratorna_dwa.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Laboratorna_dwa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
