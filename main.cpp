084cbe3c5867a836a0d02b650a6f0396ce441ac3
Rasp <naumenko.roman2003@gmail.com>
2021-02-27 15:11:13 +0200

    Add file parsing

--- main.cpp
+++ main.cpp
@@ -4,25 +4,37 @@
 #include <io.h>
 #include <vector>
 #include <cstring>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 template <typename T>
 void print_vector(vector<T>);
 string find_files(vector<string>&); // Finds files' names
-void collect_data(vector<string>&, vector<vector<string>>&, string, int&); // Collects data from all files to one vector
-float calculate_min_score(); // Calculates min score for CTEIIYXA
-void form_results(vector<vector<string>>&, string); // Forms .csv file with 40% happy dudes, who'll get CTEIIYXA
+void collect_data(vector<string>&, vector<vector<string>>&, string); // Collects data about non-contractors
+float calculate_min_score(float*,int); // Calculates min score for CTEIIYXA
+void form_results(vector<vector<string>>&, string); // Forms .csv file with 40% of happy dudes, who'll get CTEIIYXA
 
 int main() {
+    setlocale(LC_ALL, "Rus");
     vector<vector<string>> all_data; //data from all files
     vector <string> files; // Files' names
-    int students_count = 0;
+    float* average_scores;
+    string* only_names;
+    int not_contractors;
     float min_score;
     string path;
 
     path = find_files(files);
     print_vector(files);
+
+    collect_data(files, all_data, path);
+
+    not_contractors = all_data.size();
+
+    average_scores = new float[not_contractors];
+    only_names = new string[not_contractors];
 
 }
 
@@ -30,7 +42,7 @@ template <typename T>
 void print_vector(vector<T> vct) {
     for (auto i = vct.begin(); i != vct.end(); ++i)
     {
-        cout << *i << endl;
+        cout << *i << "\n";
     }
 }
 
@@ -55,4 +67,48 @@ string find_files(vector<string>& files) {
     _findclose(handle);
 
     return out_path;
+}
+
+void collect_data(vector<string>& file_names, vector<vector<string>>& data, string path){
+    ifstream file;
+    string n_str; // Number of student
+    int n; // That number, but integer
+    vector<string> temp;
+    string log;
+
+    for (auto it = file_names.begin(); it != file_names.end() ; ++it) {
+
+        file.open(path + "/" + *it);
+
+        if (!file.is_open())
+        {
+            cout << "Failed to open file '" << *it << "' " << endl;
+        }
+
+        getline(file, n_str, '\n');
+        n = stoi(n_str);
+
+        for (size_t i = 0; i < n; i++)
+        {
+            for (size_t j = 0; j < 6; j++)
+            {
+                getline(file, log, ',');
+                temp.push_back(log);
+            }
+
+            getline(file, log, '\n');
+
+            temp.push_back(log);
+            if (log == "FALSE")
+                data.push_back(temp);
+
+            temp.clear();
+        }
+
+        file.close();
+    }
+}
+
+float calculate_min_score(float* scores, int n){
+    return 0;
 }
